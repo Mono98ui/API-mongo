0865cdf5bc06765b3e24809bc70feecbd3e36d4a
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2023-12-29 01:02:38 -0500

    add cors policy to api

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+var cors = require('cors')
 const productRoute = require('./routes/productRoute')
 const errorMiddleware = require('./middleware/errorMiddleware')
 
 const MONGO_DB = process.env.MONGO_URL
 const PORT = process.env.PORT
+//const FRONT_END = process.env.FRONT_END
 
+//Specifying which website can acce
+//var corsOptions = {
+//  origin: [FRONT_END,'http://example2.com']
+//  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//}
+//app.use(corsOptions)
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
